ac66462a0554f9f0dcb050bca112dd5c3e9c4241
Pablo <pgonzaleza@academiacrespo.com>
2026-09-25 15:06:22 +0200

    Primer commit del Repository

---
+++ Codigo Python Prueba.py
@@ -0,0 +1,7 @@
+edad1 = input("CUANTOS AÑOS TIENES")
+
+edad2 = input("CUANTOS AÑOS TIENES")
+if (edad1 > edad2)
+    print("Es mayor el numero " edad1)
+else
+    print("Es mayor el numero " edad1)
